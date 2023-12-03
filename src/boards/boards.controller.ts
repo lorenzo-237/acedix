4808f3487b45d110dffeb35160ed0c3f127f53da
lorenzo <lorenzo.softdev@gmail.com>
2023-12-03 23:26:06 +0100

    implements GET, POST, lists

--- src/boards/boards.controller.ts
+++ src/boards/boards.controller.ts
@@ -1,12 +1,25 @@
-import { Controller, Get, Body, Patch, Param, Delete } from '@nestjs/common';
+import {
+  Controller,
+  Get,
+  Body,
+  Patch,
+  Param,
+  Delete,
+  Post,
+} from '@nestjs/common';
 import { BoardsService } from './boards.service';
 import { UpdateBoardDto } from './dto/update-board.dto';
 import { ApiTags } from '@nestjs/swagger';
+import { ListsService } from 'src/lists/lists.service';
+import { CreateListDto } from 'src/lists/dto/create-list.dto';
 
 @ApiTags('boards')
 @Controller('boards')
 export class BoardsController {
-  constructor(private readonly boardsService: BoardsService) {}
+  constructor(
+    private readonly boardsService: BoardsService,
+    private readonly listsService: ListsService,
+  ) {}
 
   @Get(':id')
   findOne(@Param('id') id: string) {
@@ -21,5 +34,18 @@ export class BoardsController {
   @Delete(':id')
   remove(@Param('id') id: string) {
     return this.boardsService.remove(+id);
+  }
+
+  @Get(':board_id/lists')
+  findAllLists(@Param('board_id') board_id: string) {
+    return this.listsService.findAll(+board_id);
+  }
+
+  @Post(':board_id/lists')
+  createNewList(
+    @Param('board_id') board_id: string,
+    @Body() dto: CreateListDto,
+  ) {
+    return this.listsService.create(+board_id, dto);
   }
 }
--- src/boards/boards.module.ts
+++ src/boards/boards.module.ts
@@ -1,8 +1,10 @@
 import { Module } from '@nestjs/common';
 import { BoardsService } from './boards.service';
 import { BoardsController } from './boards.controller';
+import { ListsModule } from 'src/lists/lists.module';
 
 @Module({
+  imports: [ListsModule],
   controllers: [BoardsController],
   providers: [BoardsService],
   exports: [BoardsService],
