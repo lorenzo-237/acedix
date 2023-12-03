c21f5667266c99093b4f509839720c1cd1780694
lorenzo <lorenzo.softdev@gmail.com>
2023-12-03 01:50:01 +0100

    remove: old post board

--- src/boards/boards.controller.ts
+++ src/boards/boards.controller.ts
@@ -1,30 +1,12 @@
-import {
-  Controller,
-  Get,
-  Post,
-  Body,
-  Patch,
-  Param,
-  Delete,
-} from '@nestjs/common';
+import { Controller, Get, Body, Patch, Param, Delete } from '@nestjs/common';
 import { BoardsService } from './boards.service';
-import { CreateBoardDto } from './dto/create-board.dto';
 import { UpdateBoardDto } from './dto/update-board.dto';
 import { ApiTags } from '@nestjs/swagger';
-import { Board } from './entities/board.entity';
 
 @ApiTags('boards')
 @Controller('boards')
 export class BoardsController {
   constructor(private readonly boardsService: BoardsService) {}
-
-  /**
-   * Create a new board
-   */
-  @Post()
-  create(@Body() createBoardDto: CreateBoardDto): Promise<Board> {
-    return this.boardsService.create(createBoardDto);
-  }
 
   @Get(':id')
   findOne(@Param('id') id: string) {
