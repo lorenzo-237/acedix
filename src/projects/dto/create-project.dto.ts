72c9bd6ec48debd0b9c716b15f6a40cc07fe99ca
lorenzo <lorenzo.softdev@gmail.com>
2023-12-07 00:21:47 +0100

    setup simple class validator

--- src/projects/dto/create-project.dto.ts
+++ src/projects/dto/create-project.dto.ts
@@ -1,4 +1,10 @@
+import { IsNotEmpty, IsString } from 'class-validator';
 export class CreateProjectDto {
+  @IsNotEmpty()
+  @IsString()
   name: string;
+
+  @IsNotEmpty()
+  @IsString()
   description: string;
 }
