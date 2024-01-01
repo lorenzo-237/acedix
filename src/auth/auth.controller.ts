0861c74d667f222e8197be6ad16a5e02e02b8a9b
lorenzo <lorenzo.softdev@gmail.com>
2024-01-01 19:16:56 +0100

    add auth tag for swagger

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,11 +1,13 @@
 import { Controller, Get, Post, Req, UseGuards } from '@nestjs/common';
 import { AuthService } from './auth.service';
-import { Request } from 'express';
+import { Request } from 'src/acedix/types';
 import { JwtAuthGuard } from './guards/jwt-auth.guard';
 import { SessionAuthGuard } from './guards/session-auth.guard';
 import { LocalAuthGuard } from './guards/local-auth.guard';
 import { LocalStatelessAuthGuard } from './guards/local-stateless.guard';
+import { ApiTags } from '@nestjs/swagger';
 
+@ApiTags('authentication')
 @Controller('auth')
 export class AuthController {
   constructor(private readonly authService: AuthService) {}
